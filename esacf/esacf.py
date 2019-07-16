164c7323b6294e48ec573b4719844afa95c319c2
Sevag Hanssian <sevag.hanssian@gmail.com>
2019-07-16 07:56:02 -0400

    Good breakfast

--- esacf/esacf.py
+++ esacf/esacf.py
@@ -12,7 +12,7 @@ def multipitch_esacf(audio_file_path: str):
     x = wfir(x, fs, 12)
 
     x_highpass = highpass_filter(x.copy(), fs)
-    x_highpass.clip(0) # half-wave rectification
+    x_highpass = numpy.clip(x_highpass, 0, None) # half-wave rectification
     x_highpass = lowpass_filter(x_highpass, fs) # paper wants it
 
     x_lowpass = lowpass_filter(x.copy(), fs)
@@ -20,7 +20,7 @@ def multipitch_esacf(audio_file_path: str):
     x_sacf = sacf(x_lowpass, x_highpass)
     x_esacf = esacf(x_sacf)
 
-    return x_sacf, x_esacf
+    return x, x_sacf, x_esacf
 
 
 def sacf(x_low: numpy.ndarray, x_high: numpy.ndarray) -> numpy.ndarray:
@@ -28,7 +28,7 @@ def sacf(x_low: numpy.ndarray, x_high: numpy.ndarray) -> numpy.ndarray:
     left = numpy.abs(numpy.fft.fft(x_low))**k
     right = numpy.abs(numpy.fft.fft(x_high))**k
     x2 = numpy.fft.ifft(left + right)
-    return x2
+    return numpy.real(x2)
 
 
 def esacf(x2: numpy.ndarray, n_peaks=2) -> numpy.ndarray:
@@ -37,15 +37,16 @@ def esacf(x2: numpy.ndarray, n_peaks=2) -> numpy.ndarray:
     clip to positive values, time stretch by n_peaks
     subtract original
     '''
+    x2tmp = x2.copy()
 
-    for timescale in range(n_peaks):
-        x2tmp = x2
-        x2tmp.clip(0)
-        x2stretched = librosa.effects.time_stretch(x2tmp, 1.0+timescale)
+    for timescale in range(2, n_peaks+1):
+        x2tmp = numpy.clip(x2tmp, 0, None)
+        x2stretched = librosa.effects.time_stretch(x2tmp, timescale)
+        x2stretched = numpy.pad(x2stretched, (0, x2tmp.shape[0]-x2stretched.shape[0]), 'constant')
         x2tmp -= x2stretched
-        x2tmp.clip(0)
-        x2 = x2tmp
+        x2tmp = numpy.clip(x2tmp, 0, None)
 
+    return x2tmp
 
 
 def highpass_filter(x: numpy.ndarray, fs: float) -> numpy.ndarray:
--- multipitch.py
+++ multipitch.py
@@ -1,6 +1,31 @@
 from esacf import multipitch_esacf
+import matplotlib.pyplot as plt
+import numpy
 import sys
 
 
 if __name__ == '__main__':
-    multipitch_esacf(sys.argv[1])
+    x, sacf, esacf = multipitch_esacf(sys.argv[1])
+    samples = numpy.arange(len(x))
+
+    fig1, (ax1, ax2) = plt.subplots(2, 1)
+
+    ax1.set_title('x[n] (piano chord)')
+    ax1.set_xlabel('n (samples)')
+    ax1.set_ylabel('amplitude')
+    ax1.plot(samples, x, 'b', alpha=0.8, label='x[n]')
+    ax1.grid()
+    ax1.legend(loc='upper right')
+
+    ax2.set_title('SACF, ESACF')
+    ax2.set_xlabel('n (samples)')
+    ax2.set_ylabel('amplitude')
+    ax2.plot(samples, sacf, 'g', linestyle='--', alpha=0.5, label='sacf')
+    ax2.plot(samples, esacf, 'b', linestyle=':', alpha=0.5, label='esacf')
+
+    ax2.grid()
+    ax2.legend(loc='upper right')
+
+    plt.axis('tight')
+    fig1.tight_layout()
+    plt.show()
